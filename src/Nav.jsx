a84fb07110b6bcacf41d9d47328535e7f71c536e
Josh Pearlman <82927774+joshpman@users.noreply.github.com>
2023-10-21 21:03:01 -0400

    Fixed cut-offs
    Overflow:hidden; cut off content on smaller vh screens

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,7 +1,7 @@
 const Nav = ({handleButtonClick, handleRegister, goHome})=>{
     return(
         <nav id="topNav">
-            <img id="topLogo" onClick={goHome} src="../jpLogo.webp" alt="JP"></img>
+            <img id="topLogo" onClick={goHome} src="../jpLogo.webp" alt="JP" draggable="false"></img>
             <div id="mainOptions">
                 <button page-value={0} onClick={handleButtonClick} type="button" className="navCenterButton" id="Home">Home</button>
                 <button page-value={1} onClick={handleButtonClick} type="button" className="navCenterButton" id="Features">Features</button>
